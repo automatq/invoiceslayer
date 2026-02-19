eb20a7b6bb51e6f08b961780ffb62907c126ba8d
Chris <zmatus@proton.me>
2026-02-19 18:03:53 -0500

    fix: remove duplicate userId migration, add migrate deploy to build

--- prisma/migrations/20260219230041_add_userid_to_settings/migration.sql
+++
@@ -1,5 +0,0 @@
--- Add userId column to Settings table
-ALTER TABLE "Setting" ADD COLUMN "userId" TEXT;
-
--- Create unique index on userId
-CREATE UNIQUE INDEX "Setting_userId_key" ON "Setting"("userId");
